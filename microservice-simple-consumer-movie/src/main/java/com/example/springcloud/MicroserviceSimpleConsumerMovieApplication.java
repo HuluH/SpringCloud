a3535968c85210e6120c5fd7d05f36b9cf96d31a
song <1269930730@qq.com>
2020-07-22 22:27:53 +0800

    update(消费者服务)：Ribbon做负载均衡更改负载策略

--- microservice-simple-consumer-movie/src/main/java/com/example/springcloud/MicroserviceSimpleConsumerMovieApplication.java
+++ microservice-simple-consumer-movie/src/main/java/com/example/springcloud/MicroserviceSimpleConsumerMovieApplication.java
@@ -1,5 +1,8 @@
 package com.example.springcloud;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import com.netflix.loadbalancer.RoundRobinRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
@@ -13,6 +16,15 @@ public class MicroserviceSimpleConsumerMovieApplication {
     @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    /**
+     * 更改Ribbon默认的负载均衡策略
+     */
+    @Bean
+    public IRule irule() {
+        // 实现轮询的负载均衡策略
+        return new RoundRobinRule();
     }
 
     public static void main(String[] args) {
